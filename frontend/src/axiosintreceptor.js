f515ae9d4a3298d1a4a943eb3216e6ae22a7cd65
ard3924 <aravindr3924@gmail.com>
2025-12-17 10:53:27 +0530

    updated backend package for deployment in render

--- frontend/src/axiosintreceptor.js
+++ frontend/src/axiosintreceptor.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "/api"
-    // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5200/api",
+    // baseURL: "/api"
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5200/api",
 });
 
 
