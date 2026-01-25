d16f8590a3d668b34ae60ff7b21960759c091ebd
ard3924 <aravindr3924@gmail.com>
2026-01-25 19:04:09 +0530

    changed base url to deployed backend url

--- frontend/src/axiosintreceptor.js
+++ frontend/src/axiosintreceptor.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5200/api",
+    // baseURL: "http://localhost:5200/api",
     // baseURL: "/api",
-    // baseURL: "https://localfinds-backend-e2q7.onrender.com/api",
+    baseURL: "https://localfinds-backend-e2q7.onrender.com/api",
     withCredentials: true,
 });
 
